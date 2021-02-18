0f39ec85b8f99143e77f547f91563d4200837e0f
Jere Vuola <vuolajere@gmail.com>
2021-02-18 12:43:17 +0200

    - Added navigation bar - Added new fragments - Added 3 icons

--- app/src/main/java/com/example/habitti/MainActivity.java
+++ app/src/main/java/com/example/habitti/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.habitti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Jere on paras
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new MainFragment()).commit();
+
+
     }
+
+
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment=null;
+            switch (item.getItemId())
+            {
+                case R.id.calendar:
+                    selectedFragment = new CalendarFragment();
+                    break;
+                case R.id.habits:
+                    selectedFragment = new MainFragment();
+                    break;
+                case R.id.settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
 }
