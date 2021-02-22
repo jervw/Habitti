795c0c2802077bf0bfb15cb2112c56dace1eb530
Jere Vuola <vuolajere@gmail.com>
2021-02-22 16:16:12 +0200

    Added settings (design)

--- app/src/main/java/com/example/habitti/SettingsFragment.java
+++ app/src/main/java/com/example/habitti/SettingsFragment.java
@@ -14,12 +14,10 @@ import androidx.fragment.app.Fragment;
 public class SettingsFragment extends Fragment {
 
 
-
-
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
+        }
     }
-}
