6b13f0a27045ca3e1452f0390383dec1ef7621b8
Jere Vuola <vuolajere@gmail.com>
2021-02-23 15:23:16 +0200

    better fragment_main.xml design

--- app/src/main/java/com/example/habitti/MainActivity.java
+++ app/src/main/java/com/example/habitti/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,16 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new MainFragment()).commit();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.app_bar, menu);
+        getMenuInflater().inflate(R.menu.top_bar, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
 
     private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -33,20 +36,22 @@ public class MainActivity extends AppCompatActivity {
             Fragment selectedFragment=null;
             switch (item.getItemId())
             {
-                case R.id.calendar:
-                    selectedFragment = new CalendarFragment();
-                    break;
                 case R.id.habits:
                     selectedFragment = new MainFragment();
+                    break;
+                case R.id.calendar:
+                    selectedFragment = new CalendarFragment();
                     break;
                 case R.id.settings:
                     selectedFragment = new SettingsFragment();
                     break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
-            return true;
+            if (selectedFragment != null){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                return true;
+            }
+            return false;
+
         }
-
-
     };
 }
