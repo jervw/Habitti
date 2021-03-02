a032d8d29a198eee936e35835bb9351fd6f4cd6e
Jere Vuola <vuolajere@gmail.com>
2021-03-02 17:52:03 +0200

    spinner fixed

--- app/src/main/java/com/example/habitti/AddHabitDialog.java
+++ app/src/main/java/com/example/habitti/AddHabitDialog.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.FragmentManager;
 
 public class AddHabitDialog extends AppCompatDialogFragment {
 
@@ -19,13 +22,16 @@ public class AddHabitDialog extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder((getActivity()));
-
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.new_habit_dialog, null);
 
         final ImageView dialogImageView = (ImageView) view.findViewById(R.id.habitImage);
         dialogImageView.setImageResource(R.drawable.habit_1);
 
+        Spinner habitSpinner = (Spinner) view.findViewById(R.id.spinnerHabbits);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.habbits_array));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        habitSpinner.setAdapter(adapter);
 
         builder.setView(view)
                 .setTitle("New habit")
@@ -42,6 +48,8 @@ public class AddHabitDialog extends AppCompatDialogFragment {
 
                     }
                 });
+
+
         return builder.create();
     }
 }
--- app/src/main/java/com/example/habitti/MainActivity.java
+++ app/src/main/java/com/example/habitti/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
-import android.content.Intent;
 import android.app.Notification;
 
 import android.app.NotificationChannel;
@@ -19,9 +18,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -30,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     public static final String CHANNEL_1_ID = "channel";
 
     private NotificationManagerCompat notificationManager;
-    Button addNewHabbitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +40,11 @@ public class MainActivity extends AppCompatActivity {
         notificationManager = NotificationManagerCompat.from(this);
     }
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_bar, menu);
         return super.onCreateOptionsMenu(menu);
-    }
+    }*/
 
     private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -107,19 +102,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    public void onClickAddHabbit (View view){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        View mView = getLayoutInflater().inflate(R.layout.new_habit_dialog, null);
-        builder.setTitle("Add new habit");
-        Spinner habbitSpinner = (Spinner) mView.findViewById(R.id.spinnerHabbits2);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.habbits_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        habbitSpinner.setAdapter(adapter);
-        /*Intent intent = new Intent(this, AddNewHabbits.class);
-        startActivity(intent);*/
-        builder.setView(mView);
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
 }
