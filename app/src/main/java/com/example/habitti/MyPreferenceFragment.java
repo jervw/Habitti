a9ceca30eb803c0e59cc6a09d60db97823b24262
Jere Vuola <vuolajere@gmail.com>
2021-03-05 17:00:48 +0200

    added credits

--- app/src/main/java/com/example/habitti/MyPreferenceFragment.java
+++ app/src/main/java/com/example/habitti/MyPreferenceFragment.java
@@ -1,7 +1,14 @@
 package com.example.habitti;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class MyPreferenceFragment extends PreferenceFragmentCompat {
@@ -9,5 +16,21 @@ public class MyPreferenceFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.prefs, rootKey);
+
+        Preference button = findPreference(getString(R.string.creditsButton));
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Context context = getActivity().getApplicationContext();
+                CharSequence text = "Santeri Hytönen\n Anna Raevskaia\n Jere Vuola";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                return true;
+            }
+
+        });
+
+
     }
 }
--- app/src/main/java/com/example/habitti/SettingsFragment.java
+++ app/src/main/java/com/example/habitti/SettingsFragment.java
@@ -1,11 +1,14 @@
 package com.example.habitti;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +22,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
     }
+
 }
